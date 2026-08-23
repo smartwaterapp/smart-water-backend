798ed04aac5f2591c6a36650a3d368afe62e779d
pyezen <yazan.yazan.712014@gmail.com>
2026-08-23 19:45:51 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -9,7 +9,17 @@ from datetime import datetime
 # Create database tables
 models.Base.metadata.create_all(bind=engine)
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI(title="Smart Water Backend (ThingSpeak Clone)")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # --- Pydantic Schemas for API responses ---
 class ChannelCreate(BaseModel):
